b949ebc9b999ebfd7965c345d1e53f3d80c54348
andikaputra <andika.wicaksana@igloohome.co>
2022-06-13 18:39:46 +0800

    clean code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
   },
   {
-    path: 'overview',
-    loadChildren: './overview/overview.module#OverviewPageModule',
+    path: "overview",
+    loadChildren: "./overview/overview.module#OverviewPageModule",
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
